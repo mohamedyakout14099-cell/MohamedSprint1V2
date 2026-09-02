807406891120f7d245136f6e84dad7c7d31735df
Mohamed <mohamedyakout14099>
2026-09-02 21:11:10 +0300

    adding Authorization to Product and Category And img in View and Edit in Product

--- MohamedSprint1V2.DBL/ModelVM/Product/AddProductVM.cs
+++ MohamedSprint1V2.DBL/ModelVM/Product/AddProductVM.cs
@@ -1,4 +1,4 @@
-﻿namespace MohamedSprint1V2.DLL.ModelVM.Product
+namespace MohamedSprint1V2.DLL.ModelVM.Product
 {
     public class AddProductVM
     {
@@ -9,12 +9,10 @@
         [Required(ErrorMessage = "Description is required")]
         [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters")]
         public string Description { get; set; }
-        [Required(ErrorMessage = "Image URL is required")]
-        [StringLength(200, ErrorMessage = "Image URL cannot exceed 200 characters")]
-        public string Img { get; set; }
+        public string? Img { get; set; }
 
         [Required(ErrorMessage = "Price is required")]
-        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be a positive number")]
         public decimal Price { get; set; }
         [Required(ErrorMessage = "Category is required")]
         public int CategoryId { get; set; }
